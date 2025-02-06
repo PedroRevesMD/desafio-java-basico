811d7aa712e474c4c5fe2c1a9f288079fe43a492
Pedro Reves <pedroreves11@gmail.com>
2025-02-06 18:35:00 -0300

    feat: receiving parameters from terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
         String nomeCliente;
         int numeroConta;
-        String agenciaConta;
+        String numeroAgencia;
         double saldoConta;
+
+        System.out.println("Olá, Seja bem Vindo ao nosso banco!");
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.forLanguageTag("pt-BR"))) {
+            
+            System.out.println("Por favor, Digite o seu Nome:");
+            nomeCliente = scanner.nextLine();
+
+            System.out.println("Por favor, Digite o numero da sua agência:");
+            numeroAgencia = scanner.next();
+
+            System.out.println("Por favor, Digite o numero da sua Conta Bancária:");
+            numeroConta = scanner.nextInt();
+
+            System.out.println("Por favor, Digite o numero do Saldo Inicial:");
+            saldoConta = scanner.nextDouble();
+        }
     }
 }
