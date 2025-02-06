98e468a40017743c5d65c1f10c624d357bb56977
Pedro Reves <pedroreves11@gmail.com>
2025-02-06 18:12:58 -0300

    creation of main variables

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String nomeCliente;
+        int numeroConta;
+        String agenciaConta;
+        double saldoConta;
     }
 }
